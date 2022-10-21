8cb3c293329a941211f689c95411996b98186733
kingkazuma7 <your.nasionasio7@gmail.com>
2022-10-21 20:09:24 +0900

    33

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
+import { Chat, Notifications, Search } from "@mui/icons-material";
+import "./Topbar.css";
 
 export default function Topbar() {
-  return (
-    <div className='topbarContainer'>
-      <div className='topbarLeft'>
-        <span className='logo'>Real SNS</span>
-      </div>
-      <div className='topbarCenter'>
-        <div className='searchbar'>
-          <input type="text" className='seach' placeholder='探しものはなんですか' />
+return (
+<div className='topbarContainer'>
+  <div className='topbarLeft'>
+    <span className='logo'>Real SNS</span>
+  </div>
+  <div className='topbarCenter'>
+    <div className='searchbar'>
+      <Search className='searchIcon' />
+      <input type="text" className='seachInput' placeholder='探しものはなんですか' />
+    </div>
+  </div>
+  <div className='topbarRight'>
+    <div className="topbarIconItem">
+      <div className="topbarItemIcons">
+        <div className="topbariconItem">
+          <Chat />
+          <span className="topbarIconBadge">1</span>
+        </div>
+        <div className="topbariconItem">
+          <Notifications />
+          <span className="topbarIconBadge">2</span>
         </div>
       </div>
-      <div className='topbarRight'>
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
-      </div>
-      <img src="/assets/person/1.jpeg" alt="" className='topbarImg' />
     </div>
-  )
+    <img src="/assets/person/1.jpeg" alt="" className='topbarImg' />
+  </div>
+</div>
+)
 }
